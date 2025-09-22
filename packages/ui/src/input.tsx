87c229b1c19118342dfdd628cba932d3d91d1f3a
Aron <86988185+nokika0@users.noreply.github.com>
2025-09-22 01:46:30 -0700

    add input lab (#1)

---
+++ packages/ui/src/input.tsx
@@ -0,0 +1,46 @@
+import { getInputSizeStyles, Size } from './size'
+import { getCommonStyles } from './tokens'
+import { getVariantBorderStyles, getVariantInputTextStyles, getVariantOutlineStyles, Variant } from './variant' // added to give input the background styles
+import { HTMLInputTypeAttribute } from 'react'
+
+interface InputProps {
+  variant?: Variant
+  size?: Size
+  placeholder?: string
+  type?: string
+  value?: any
+  setValue?: (newValue: any) => void
+  defaultValue?: any
+  name: string
+  id: string
+}
+
+export default function Input({
+  variant = Variant.PRIMARY,
+  size = Size.MEDIUM,
+  value,
+  name,
+  id,
+  defaultValue,
+  setValue,
+  type = 'text',
+  placeholder,
+}: InputProps) {
+  const sizeCssClasses = getInputSizeStyles(size)
+  const variantBorderCssClasses = getVariantBorderStyles(variant)
+  const variantInputTextCssClasses = getVariantInputTextStyles(variant)
+  const variantOutlineCssClasses = getVariantOutlineStyles(variant)
+  const commonCssClasses = getCommonStyles()
+  return (
+    <input
+      className={` ${sizeCssClasses} ${variantBorderCssClasses} ${variantInputTextCssClasses} ${variantOutlineCssClasses} ${commonCssClasses}`}
+      name={name}
+      id={id}
+      defaultValue={defaultValue}
+      placeholder={placeholder}
+      type={type}
+      value={value}
+      onChange={setValue ? (newValue) => setValue(newValue.currentTarget.value) : () => {}}
+    />
+  )
+}
