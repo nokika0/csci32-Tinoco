12a9761f20fbe32276b6912ad2c567bf40e5019a
Aron T <arontinoco305@gmail.com>
2025-10-05 23:32:42 -0700

    prisma integration

--- packages/ui/src/math.ts
+++ packages/ui/src/math.ts
@@ -3,3 +3,4 @@ export function getRandomInt({ min, max }: { min: number; max: number }) {
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
+// TODO: get this package into it's own package outside of UI
